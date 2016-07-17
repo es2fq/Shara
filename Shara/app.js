6d695f20a9914f5e6bc35cc148deb04be0bb9ce9
Erik Song <Erik Song>
2016-07-17 13:55:26 -0400

    -created dialog box that appears on load

--- Shara/app.js
+++ Shara/app.js
@@ -167,7 +167,74 @@ function deleteMarker(id)
     marker.setMap(null);
 }
 
+function showWindow()
+{
+    $(document).ready(function () {
+
+        //Get the A tag
+        var id = '#introDialog';
+
+        //Get the screen height and width
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
+
+        //transition effect
+        $('#mask').fadeTo("slow", 0.8);
+
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+
+        //Set the popup window to center
+        $(id).css('top', winH / 2 - $(id).height() / 2);
+        $(id).css('left', winW / 2 - $(id).width() / 2);
+
+        //transition effect
+        $(id).fadeIn(1000);
+
+        //if close button is clicked
+        $('.window .close').click(function (e) {
+            //Cancel the link behavior
+            e.preventDefault();
+            $('#mask, .window').hide();
+        });
+
+        //if mask is clicked
+        $('#mask').click(function () {
+            $(this).hide();
+            $('.window').hide();
+        });
+
+        $(window).resize(function () {
+
+            var box = $('#boxes .window');
+
+            //Get the screen height and width
+            var maskHeight = $(document).height();
+            var maskWidth = $(window).width();
+
+            //Set height and width to mask to fill up the whole screen
+            $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
+
+            //Get the window height and width
+            var winH = $(window).height();
+            var winW = $(window).width();
+
+            //Set the popup window to center
+            box.css('top', winH / 2 - box.height() / 2);
+            box.css('left', winW / 2 - box.width() / 2);
+
+        });
+    });
+}
+
 window.onload = function () {
+    
+    showWindow();
+
     $(function () {
         $('body').removeClass('fade-out');
     });
