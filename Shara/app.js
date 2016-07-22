86974fdec6dc2958dfa70bf687029f6fd1e0bf20
Erik Song <Erik Song>
2016-07-22 19:51:37 -0400

    -changed to url variable

--- Shara/app.js
+++ Shara/app.js
@@ -405,7 +405,7 @@ function publishStory() {
     var params = "title=" + title + "&story=" + story + "&lat=" + lat + "&lng=" + lng;
 
     $.ajax({
-        url: "https://es2fq.github.io/Shara/Shara/publish.php",
+        url: url,
         type: 'POST',
         data: params,
         success: function () {
